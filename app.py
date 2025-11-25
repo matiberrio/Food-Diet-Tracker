985bc36b95df374f3ab695d906b7acfa9e38cc2f
Matias Berrio <matiasberrio@usf.edu>
2025-11-24 23:29:23 -0500

    Task 2: Add user input prompt

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 # Task 1: Welcome message
 print("Welcome to the Diet Tracker!")
+
+# Task 2: Ask for input
+meal_name = input("Enter the name of the meal (e.g., Breakfast, Lunch, Snack): ")
+nutrient_input = input(f"Enter the amount of protein (in grams) for {meal_name}: ")
