1a2d6fd911c765b5241cc669727f82274f668478
Matias Berrio <matiasberrio@usf.edu>
2025-11-24 23:32:47 -0500

    Task 3: Add calculation for estimated daily protein

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ print("Welcome to the Diet Tracker!")
 # Task 2: Ask for input
 meal_name = input("Enter the name of the meal (e.g., Breakfast, Lunch, Snack): ")
 nutrient_input = input(f"Enter the amount of protein (in grams) for {meal_name}: ")
+
+# Task 3: Convert input & perform a calculation
+protein_grams = float(nutrient_input)
+estimated_daily_protein = protein_grams * 3 # Calculation: assume 3 meals per day.
